e39073f12c40d6e6ccd9f9b48e69e6160a86b88f
Dylan Francis <dylfrancis505@gmail.com>
2022-12-19 10:10:53 -0500

    Add game function to play 5 rounds of rock paper scissors

--- index.js
+++ index.js
@@ -43,5 +43,31 @@ let singleRound = (playerSelection, computerSelection) => {
     }
 }
 
-const userChoice = prompt("Enter rock paper or scissors!");
-console.log(singleRound(userChoice, getComputerChoice()));
+let game = () => {
+    let userScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let userChoice = prompt("Enter rock paper or scissors!");
+        const computerChoice = getComputerChoice();
+
+        let round = singleRound(userChoice, computerChoice);
+        console.log(round);
+
+        if (round === `You win! ${userChoice.toUpperCase()} beats ${computerChoice}`) {
+            userScore++;
+        } else if (round === `You lose! ${computerChoice} beats ${userChoice.toUpperCase()}`) {
+            computerScore++;
+        }
+    }
+
+    if (userScore > computerScore) {
+        console.log(`You win!\n\nSCORE:\nUser: ${userScore}\nComputer: ${computerScore}`);
+    } else if (userScore < computerScore) {
+        console.log(`You lose.\n\nSCORE:\nUser: ${userScore}\nComputer: ${computerScore}`)
+    } else {
+        console.log(`You tied!\n\nSCORE:\nUser: ${userScore}\nComputer: ${computerScore}`)
+    }
+}
+
+game();
