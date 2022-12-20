4eae139d089b23549658eecf5bcf446f2fc6f7ab
Dylan Francis <dylfrancis505@gmail.com>
2022-12-20 12:45:09 -0500

    Add main page containers, add music on start button press

--- start.js
+++ start.js
@@ -1,9 +1,15 @@
+let startMusic = () => {
+  const music = document.querySelector(".background-music");
+  music.play();
+};
+
 let pressStart = () => {
   const startPage = document.querySelector(".start-page");
   startPage.style.display = "none";
 };
 
 const startButton = document.querySelector(".start-btn");
-startButton.addEventListener("click", function () {
+startButton.addEventListener("click", () => {
   pressStart();
+  startMusic();
 });
